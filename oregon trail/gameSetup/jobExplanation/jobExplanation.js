f68bfe090347783a9f480add978f93246bf533f5
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2023-12-12 16:11:02 +0000

    changed oregon trail to maybe work

--- oregon trail/gameSetup/jobExplanation/jobExplanation.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var spacebarKey = 32;
-    $(document).keydown(function(e){
-        if(e.keyCode == spacebarKey){
-            location.replace("../jobSetup.html");
-        }
-    });
-});
--- oregon trail/homescreenPages/managementOptions/topTen/topTen.js
+++
@@ -1,7 +0,0 @@
-document.body.onkeyup = function(e){
-	if(e.keyCode == spacebarKey){ // spacebar
-		location.replace("../managementOptions.html");
-	}
-}
-
-var spacebarKey = 32;
