2192e1c80adaec455b4f4ea6b12dedfd114243c1
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2023-12-14 17:27:07 +0000

    changed webrcade editor

--- webrcade editor/src/components/common/ToolbarVerticalDivider.js
+++
@@ -1,8 +0,0 @@
-
-import Divider from '@mui/material/Divider';
-
-export default function ToolbarVerticalDivider() {
-  return (
-    <Divider orientation="vertical" flexItem variant="middle" sx={{ marginLeft: 1, marginRight: 1 }} />
-  );
-}
